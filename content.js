61254fbb741a6eadcc083f559f4894e0fdba482c
Uğur ALBAYRAK <mahfuz__@hotmail.com>
2024-11-15 12:04:03 +0300

    ?

--- content.js
+++ content.js
@@ -1,26 +1,132 @@
 function updateNote() {
     // "rel" attribute'u "odenecekTutar" olan td elementini bul
-    let odenecekTutarElement = document.querySelector('td[rel="odenecekTutar"] input');
+    var odenecekTutarElement = $('td[rel="odenecekTutar"] input');
 
-    if (odenecekTutarElement) {
-        let valueToCopy = odenecekTutarElement.value;
-        var num = Math.abs(Number(valueToCopy.replaceAll(".", "").replace(",", ".")));
+    if (odenecekTutarElement.length) {
+        var tutar = odenecekTutarElement.val();
+        var num = Math.abs(Number(tutar.replaceAll(".", "").replace(",", ".")));
         var txtRes = num2TextPart(num, "TL ") + num2TextPart(((num * 100) % 100).toFixed(), "Kr", true);
 
         // "rel" attribute'u "not" olan div elementini bul
-        let notElement = document.querySelector('div[rel="not"] textarea');
+        var notElement = $('div[rel="not"] textarea');
 
-        if (notElement) {
-            var openBracket = notElement.value.indexOf("Yalnız ");
-            if (openBracket == -1) notElement.value += "Yalnız " + txtRes.trim() + "'dir.";
+        if (notElement.length) {
+            var not = notElement.val();
+            var yaziStart = not.indexOf("Yalnız ");
+            if (yaziStart == -1) not += "Yalnız " + txtRes.trim() + "'dir.";
             else {
-                var closeBracket = notElement.value.indexOf("'dir.", openBracket) + 5;
-                notElement.value = notElement.value.substring(0, openBracket) +
-                    "Yalnız " + txtRes.trim() + "'dir." +
-                    notElement.value.substring(closeBracket);
+                var yaziEnd = not.indexOf("'dir.", yaziStart) + 5;
+                not = not.substring(0, yaziStart) + "Yalnız " + txtRes.trim() + "'dir." + not.substring(yaziEnd);
             }
-            loadUserNote(notElement);
+
+            var cmdEditNoteBtn = $("#cmdEditNote");
+            if (cmdEditNoteBtn.length == 0) {
+                notElement.before(`<button style="margin-bottom: 8px;" type="button" id="cmdEditNote" class="csc-button">Notu Düzenle</button>`);
+                cmdEditNoteBtn = $("#cmdEditNote");
+            }
+            cmdEditNoteBtn.off("click").on("click", editUserNote);
+
+            var userIDElement = $('div[rel="userID"] p');
+
+            if (userIDElement.length) {
+                const userID = userIDElement.text();
+                loadUserNote(userID).then((userNote) => {
+                    const userNoteText = userNote.text;
+                    const userNoteTitle = userNote.title;
+                    if (userNoteText != "" && not.indexOf(userNoteTitle + ":") == -1) not += "\n\n" + userNoteTitle + ":\n" + userNoteText;
+                    notElement.val(not);
+                });
+            }
         }
+    }
+}
+
+function loadUserNote(userID) {
+    return new Promise((resolve) => {
+        chrome.storage.local.get(["userNotes"], function (data) {
+            const userNotes = data.userNotes || {};
+            const userNote = userNotes[userID] || { title: '', text: '' };
+            resolve(userNote);
+        });
+    });
+}
+
+function saveUserNote() {
+    const userIDElement = $('div[rel="userID"] p');
+    if (userIDElement.length) {
+        const userID = userIDElement.text();
+        const userNoteText = $("#userNoteText").val() ?? '';
+        const userNoteTitle = $("#userNoteTitle").val() ?? '';
+
+        chrome.storage.local.get(["userNotes"], function (data) {
+            const userNotes = data.userNotes || {};
+            userNotes[userID] = { title: userNoteTitle, text: userNoteText };
+
+            chrome.storage.local.set({ userNotes }, function () {
+                alert("Not başarıyla kaydedildi.");
+                $('#dlgEditNote').fadeOut();
+            });
+        });
+    }
+}
+
+function editUserNote() {
+    const userIDElement = $('div[rel="userID"] p');
+    if (userIDElement.length) {
+        const userID = userIDElement.text();
+
+        loadUserNote(userID).then((userNote) => {
+            const userNoteText = userNote.text;
+            const userNoteTitle = userNote.title;
+
+            let editNoteDialog = $("#dlgEditNote");
+            if (editNoteDialog.length == 0) {
+                $(document.body).prepend(`
+                    <div id="dlgEditNote" style="display: none;z-index: 2000; background-color: rgba(0, 0, 0, 0.5); position: fixed; inset: 0px;">
+                        <div
+                            style="background-color: #ffffff;position: absolute;top: 50%;left: 50%;transform: translate(-50%, -50%);border: 1px solid #999;">
+                            <div style="width: 100%;padding: 8px;background-color: #999;">
+                                <span style="font-weight: bold;">Notu Düzenle</span>
+                            </div>
+                            <div style="padding: 16px;width: 286px;height: 194px;">
+                                <table style="width: 100%;">
+                                    <tbody>
+                                        <tr>
+                                            <td><label for="userNoteTitle">Başlık:</label></td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <input type="text" id="userNoteTitle"
+                                                    style="width: 250px;margin-bottom: 8px;margin-top: 4px;">
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td><label for="userNoteText">Not:</label></td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <textarea id="userNoteText" style="width: 250px;height: 100px;margin-top: 4px;"></textarea>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                            <div
+                                style="width: 100%;padding: 16px;background-color: lightgray;border-top: 1px solid #999;display: flex;flex-flow: row-reverse;">
+                                <button onclick="$('#dlgEditNote').fadeOut()" style="padding: 8px 16px;margin-left: 16px;">İptal</button>
+                                <button id="cmdSaveUserNote" style="padding: 8px 16px;">Kaydet</button>
+                            </div>
+                        </div>
+                    </div>
+                `);
+                $("#cmdSaveUserNote").off("click").on("click", saveUserNote);
+                editNoteDialog = $("#dlgEditNote");
+            }
+
+            $("#userNoteTitle").val(userNoteTitle);
+            $("#userNoteText").val(userNoteText);
+            editNoteDialog.fadeIn();
+        });
     }
 }
 
@@ -71,11 +177,10 @@ function updateUserList(userList) {
 function loadUserList() {
     getUserList(userList => {
         if (userList.length === 0) {
-            alert("Henüz kayıtlı kullanıcı yok.");
             return;
         }
 
-        let listHTML = '<select id="userSelect" style="display: block; margin-bottom: 16px; border-bottom: 1px solid;"><option value="">Kullanıcı Seç</option>';
+        var listHTML = '<select id="userSelect" style="display: block; margin-bottom: 32px; border-bottom: 1px solid;"><option value="">Kullanıcı Seç</option>';
         userList.forEach(user => {
             listHTML += `<option value="${user.username}">${user.identifier}</option>`;
         });
@@ -191,9 +296,11 @@ $(document).ready(function () {
         loadUserList();
 
         $('#userid').after(`
+            <div style="margin-bottom: 32px">
             <button class="btn waves-effect waves-light" id="saveUserButton">Kullanıcıyı Kaydet</button>
             <button style="margin-left: 16px;" class="btn waves-effect waves-light" id="editUserButton">Kullanıcıyı Düzenle</button>
             <button style="margin-left: 16px;" class="btn waves-effect waves-light" id="deleteUserButton">Kullanıcıyı Sil</button>
+            </div>
         `);
 
         $('#saveUserButton').on('click', saveUser);
@@ -202,18 +309,3 @@ $(document).ready(function () {
     }
     else setInterval(updateNote, 500);
 });
-
-function loadUserNote(notElement) {
-    let documentBody = $(document.body);
-    let cmdEditNoteBtn = documentBody.find("#cmdEditNote");
-    if (cmdEditNoteBtn.length == 0) {
-        $(notElement).before(`<button type="button" id="cmdEditNote" class="csc-button">Notu Düzenle</button>`);
-        cmdEditNoteBtn = documentBody.find("#lstNotes");
-    }
-
-    let userIDElement = document.querySelector('div[rel="userID"] p');
-
-    if (userIDElement) var userID = userIDElement.innerText;
-
-    // rest of the code...
-}
