613bf8f8c23a9e70d6708d700896242513bd2e08
Uğur ALBAYRAK <mahfuz__@hotmail.com>
2024-11-19 14:16:46 +0300

    ?

--- usernotefunctions.js
+++ usernotefunctions.js
@@ -8,8 +8,9 @@ function updateNoteUI() {
         if (cmdEditNoteBtn.length == 0) {
             notElement.before(`<button type="button" id="cmdEditNote" class="margin-bottom-8 csc-button">Notu Düzenle</button>`);
             cmdEditNoteBtn = $("#cmdEditNote");
+            cmdEditNoteBtn.off("click").on("click", editUserNote);
         }
-        cmdEditNoteBtn.off("click").on("click", editUserNote);
+        
 
         const userIDElement = $('div[rel="userID"] p');
 
@@ -39,8 +40,8 @@ function saveUserNote() {
     const userIDElement = $('div[rel="userID"] p');
     if (userIDElement.length) {
         const userID = userIDElement.text();
-        const userNoteText = $("#userNoteText").val() ?? '';
-        const userNoteTitle = $("#userNoteTitle").val() ?? '';
+        const userNoteText = $("#txtUserNoteText").val() ?? '';
+        const userNoteTitle = $("#txtUserNoteTitle").val() ?? '';
 
         chrome.storage.local.get(["userNotes"], function (data) {
             const userNotes = data.userNotes || {};
@@ -58,42 +59,42 @@ function editUserNote() {
     let editNoteDialog = $("#dlgEditNote");
     if (editNoteDialog.length == 0) {
         $(document.body).prepend(`
-            <div id="dlgEditNote">
-                <div class="dialog-frame">
-                    <div class="dialog-header padding-8">
-                        <span>Notu Düzenle</span>
-                    </div>
-                    <div class="padding-16">
-                        <table style="width: 100%;">
-                            <tbody>
-                                <tr>
-                                    <td><label for="userNoteTitle">Başlık:</label></td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <input class="margin-bottom-8 margin-top-4 padding-4" type="text" id="userNoteTitle"
-                                            style="width: 100%;">
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td><label for="userNoteText">Not:</label></td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <textarea id="userNoteText" class="margin-top-4 padding-4"
-                                            style="width: 350px;height: 152px;resize: none;"></textarea>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                    <div class="dialog-footer padding-8-16">
-                        <button class="margin-left-16 padding-8-16" onclick="$('#dlgEditNote').fadeOut()">İptal</button>
-                        <button class="padding-8-16" id="cmdSaveUserNote">Kaydet</button>
+                <div id="dlgEditNote" class="dialog modal-layer">
+                    <div class="dialog-frame">
+                        <div class="dialog-header padding-8">
+                            <span>Notu Düzenle</span>
+                        </div>
+                        <div class="padding-16">
+                            <table style="width: 100%;">
+                                <tbody>
+                                    <tr>
+                                        <td><label for="txtUserNoteTitle">Başlık:</label></td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <input class="margin-bottom-8 margin-top-4 padding-4" type="text" id="txtUserNoteTitle"
+                                                style="width: 100%;">
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td><label for="txtUserNoteText">Not:</label></td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <textarea id="txtUserNoteText" class="margin-top-4 padding-4"
+                                                style="width: 350px;height: 152px;resize: none;"></textarea>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        <div class="dialog-footer padding-8-16">
+                            <button class="margin-left-16 padding-8-16" onclick="$('#dlgEditNote').fadeOut()">İptal</button>
+                            <button class="padding-8-16" id="cmdSaveUserNote">Kaydet</button>
+                        </div>
                     </div>
                 </div>
-            </div>
-        `);
+            `);
         $("#cmdSaveUserNote").off("click").on("click", saveUserNote);
         editNoteDialog = $("#dlgEditNote");
     }
@@ -101,8 +102,8 @@ function editUserNote() {
     const userIDElement = $('div[rel="userID"] p');
     if (userIDElement.length) {
         loadUserNote(userIDElement.text()).then((userNote) => {
-            $("#userNoteTitle").val(userNote.title);
-            $("#userNoteText").val(userNote.text);
+            $("#txtUserNoteTitle").val(userNote.title);
+            $("#txtUserNoteText").val(userNote.text);
             editNoteDialog.fadeIn();
         });
     }
